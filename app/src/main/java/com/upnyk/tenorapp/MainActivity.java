30773915ea526e85cf6fba675eac3f06a0242163
Yohanes <adeo476@gmail.com>
2021-06-21 19:50:46 +0700

    setup bottom navigation

--- app/src/main/java/com/upnyk/tenorapp/MainActivity.java
+++ app/src/main/java/com/upnyk/tenorapp/MainActivity.java
@@ -1,14 +1,54 @@
 package com.upnyk.tenorapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.menu_item_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.menu_item_search:
+                fragment = new SearchFragment();
+                break;
+            case R.id.menu_item_favorite:
+                fragment = new FavoriteFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_frame, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
